c3e60dba427414ec84136d6c6dfabe1d437a8357
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-03 21:02:56 +0900

    HeaderコンポーネントにfortAwesomeを追加する
    LinkとFormの追加

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faSearch } from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-      Header
+      <div>
+        <Link to="/">VideoTude</Link>
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="検索" />
+          <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+        </form>
+      </div>
     </div>
   )
 }
