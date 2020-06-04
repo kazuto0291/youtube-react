ca0f36fb863ebeb44831e32bc3b1cf7c0e8aeab2
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-04 19:45:13 +0900

    取得したidを使ってAPIから動画詳細を取得する
    asyncを使ってリクエスト用のメソッドを用意する。
    fetchPopularDataとfetchSelectedDataの
    共通部分をparamsで定義する。
    paramsをスプレッド構文で展開する。

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
+import VideoDetaill from '../components/VideoDetaill/VideoDetaill'
 
 const Watch = () => {
   return (
     <Layout>
-      watch page
+      <VideoDetaill />
     </Layout>
   )
 }
