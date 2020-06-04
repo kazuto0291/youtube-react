c3144ac2490f06fb455173688c68646213b319c3
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-05 01:22:11 +0900

    Create SideList.jsx
    ①SideListコンポーネントを作成し、StoreaとfetchRelatedDateをインポートする。

---
+++ src/components/SideList/SideList.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useContext} from 'react'
+import {Store} from '../../store/index'
+import {fetchRelatedData} from '../../apis/index'
+
+const SideList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SideList
