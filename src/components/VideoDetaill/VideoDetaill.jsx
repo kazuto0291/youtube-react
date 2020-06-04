d56dc2d3f3d3008711ff28927b656feab108d00f
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-04 21:55:49 +0900

    dispatch関数を使って、Global Stateに受け取ったレスポンスをセットする。
    shift関数を使って、受け取ったレスポンスからitemのみを抽出する。
    useContextを使って、globalStateとsetGlobalState（dispatch関数）を取得する。

--- src/components/VideoDetaill/VideoDetaill.jsx
+++ src/components/VideoDetaill/VideoDetaill.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import {useLocation} from 'react-router-dom'
 import {fetchSelectedData} from '../../apis/index'
+import { Store} from '../../store/index'
 
 const VideoDetaill = () => {
+  const {globalState, setGlobalState} = useContext(Store)
   const location = useLocation();
   const setSelectedVideo = async () => {
     const searchParams = new URLSearchParams(location.search)
     const id = searchParams.get('v')
     await fetchSelectedData(id).then((res) => {
+      const item = res.data.items.shift()
+      setGlobalState({type: 'SET_SELECTED', payload: {selected: item}})
       console.log('res', res);
     })
   }
