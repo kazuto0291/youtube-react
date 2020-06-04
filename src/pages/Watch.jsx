8f9eec6ff1f5fd19cd4b64c5e46414e75c2fe59d
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-05 01:53:35 +0900

    取得データのリスト表示
    globalState.related があるときにmap関数を利用してリスト表示する。
    SideListItemコンポーネントにid.key.src.titleを渡す。

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 import VideoDetaill from '../components/VideoDetaill/VideoDetaill'
+import SideList from '../components/SideList/SideList'
 
 const Watch = () => {
   return (
     <Layout>
       <VideoDetaill />
+      <SideList />
     </Layout>
   )
 }
