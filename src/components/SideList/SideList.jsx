fb7ca6037096b3dc8ca0b417fb006955a1f923ec
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-05 01:40:46 +0900

    関連データの取得
    ①useContextとStoreを使って、globalStateとdispatch関数(setGlobalState)を取得する。
    ②データを取得してGlobal Stateにセットする関数、setRelatedVideoを用意する。
    非同期関数で引数はidを受け取る。
    ③fetchRelatedDataを使ってAPIからデータを取得する。取得したデータをsetGlobalState関数を使ってStoreに保存する。
    ④useEffect 関数内で、setRelatedVideo関数を実行する。

--- src/components/SideList/SideList.jsx
+++ src/components/SideList/SideList.jsx
@@ -3,6 +3,16 @@ import {Store} from '../../store/index'
 import {fetchRelatedData} from '../../apis/index'
 
 const SideList = () => {
+  const {globalState, setGlobalState} = useContext(Store)
+  const setRelatedVideo = async (id) => {
+    await fetchRelatedData(id).then((res) => {
+      setGlobalState({type: 'SET_RELATED', payload: {related: res.data.items}})
+    })
+  }
+  useEffect(() => {
+    setRelatedVideo(globalState.selected.id)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [globalState.selected])
   return (
     <div>
       
