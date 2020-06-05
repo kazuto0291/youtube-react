8bcba89b400f6ab6dce30708b1b57426c935ae89
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-06 02:14:09 +0900

    フォームにonSubmit関数の作成
    handleSubmit関数の作成。イベントを「e」として受け取り、デフォルトのフォームの挙動を制御する。
    useHistoryをインポートして、historyオブジェクトを作成する。
    historyを利用して、検索クエリに検索ワードを含めて検索ページに遷移させる。

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import {Link} from 'react-router-dom'
 import Style from './Header.module.scss'
+import {useHistory} from 'react-router-dom'
 
 const Header = () => {
   const [term, setTerm] = useState('')
+  const history = useHistory()
+  const handleSubmit = e => {
+    e.preventDefault()
+    history.push(`/seach?query=${term}`)
+  }
   return (
     <div className={Style.header}>
       <div className={Style.item}>
         <Link to="/">VideoTude</Link>
       </div>
       <div className={Style.item}>
-        <form>
+        <form onSubmit={ handleSubmit}>
           <input
             type="text"
             placeholder="検索"
