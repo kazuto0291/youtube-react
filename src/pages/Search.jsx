992be8d7538ec7cbf1f5fa91805e4e9a47787790
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-06 17:49:33 +0900

    seachpageコンポーネントからsetSearchResult関数からfetchsearchData関数を呼び出す。
    fetchSearchData関数の引数はqueryとして追加する。そして、検索結果には、setGlobalState関数を使ってGlobalStateに保存する。
    setGlobal関数を呼び出すためにStoreとuseContextをインポートする。
    useContextとStoreを使って、globalStateとsetGloablState関数を分割代入で呼び出す。

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,21 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useContext} from 'react'
 import Layout from '../components/Layout/Layout'
 import { useLocation} from 'react-router-dom'
+import { fetchSearchData } from '../apis'
+import { Store } from '../store/index'
 
 const Search = () => {
+  const {gloalState, setGlobalState} = useContext(Store)
   const location = useLocation()
   const setSearchResult = async () => {
     const searchParams = new URLSearchParams(location.search)
     const query = searchParams.get('query')
     console.log(query)
+    if (query) {
+      await fetchSearchData(query).then((res) => {
+        setGlobalState({type: 'SET_SEARCHED', payload: {searched: res.data.items}})
+      })
+    }
   }
   useEffect(() => {
     setSearchResult()
