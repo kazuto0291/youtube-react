c1c0645b17a3f7c340aec67f76733bd9db1bed20
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-06 14:06:08 +0900

    serchpageから検索のクエリを取得する
    serchpageから検索のクエリを取得するために,useLocationをインポートする。Locationオブジェクトを作成し、クエリパラメータを取得する。setSearchResult関数内で、処理を行う。
    serchPramsのget関数にqueryを指定してパラメータを取得する。
    パラメータが取得できたか確認するためにconsole.logで確認する

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
+import { useLocation} from 'react-router-dom'
 
-const Search
- = () => {
+const Search = () => {
+  const location = useLocation()
+  const setSearchResult = async () => {
+    const searchParams = new URLSearchParams(location.search)
+    const query = searchParams.get('query')
+    console.log(query)
+  }
+  useEffect(() => {
+    setSearchResult()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <Layout>
       Seach page
