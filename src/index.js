63f331bb1fb457febf33e22642be98d9d3fcc464
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-13 20:29:13 +0900

    修正

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import 'normalize.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { StoreProvider } from './store/index'
+import {StoreProvider} from './store/index'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StoreProvider>
+            <App/>
+        </StoreProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
